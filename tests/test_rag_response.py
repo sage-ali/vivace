2fee869bd251fd4717195226fdf22f3f42769571
Ali Agboola <81039794+sage-ali@users.noreply.github.com>
2025-02-21 20:15:09 +0000

    fix(test): Add test adjustment and test CI

--- tests/test_rag_response.py
+++ tests/test_rag_response.py
@@ -4,12 +4,8 @@ from logic.services.rag_response import generate_response
 
 def test_generate_response():
     query = "What is RAG?"
-    similar_docs = [
-        "RAG stands for Retrieval-Augmented Generation.",
-        "It is a method to improve the quality of generated responses."
-    ]
+    knowledge_base_url = "https://aws.amazon.com/what-is/retrieval-augmented-generation/"
 
-    response = generate_response(query, similar_docs,
-                                       "https://aws.amazon.com/what-is/retrieval-augmented-generation/", 500)
+    response = generate_response(query, knowledge_base_url)
     assert isinstance(response, str)
     assert "RAG" in response
